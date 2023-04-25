7f06c0c19f41bc6cc040e6e0f1e062b109ff1a7d
kristianrafuse <kristianrafuse@gmail.com>
2023-04-25 14:37:21 -0300

    Update Data Analysis - SQL-challenge.sql

--- Data Analysis - SQL-challenge.sql
+++ Data Analysis - SQL-challenge.sql
@@ -38,6 +38,22 @@ JOIN dept_emp as de ON e.emp_no = de.emp_no
 JOIN departments as d ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales';
 
+-- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development')
+
+-- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+SELECT last_name, COUNT(*) as frequency_count
+FROM employees
+GROUP BY last_name
+ORDER BY frequency_count DESC;
+
+
 --TABLE REFERENCES
 --select * from departments
 --select * from employees
