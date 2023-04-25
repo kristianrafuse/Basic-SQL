febc50125bf6bb7fc1b4c3cc079a7acc8a1800ab
kristianrafuse <kristianrafuse@gmail.com>
2023-04-25 11:40:46 -0300

    Update Work - SQL-challenge.sql

--- Work - SQL-challenge.sql
+++ Work - SQL-challenge.sql
@@ -10,6 +10,20 @@ SELECT e.last_name, e.first_name, e.hire_date
 FROM employees as e
 WHERE hire_date BETWEEN '1986-01-01' and '1986-12-31'
 
+-- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager AS dm
+INNER JOIN departments AS d ON dm.dept_no = d.dept_no
+INNER JOIN employees AS e ON dm.emp_no = e.emp_no;
+
+-- 4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
+
 --TABLE REFERENCES
 --select * from departments
 --select * from employees
